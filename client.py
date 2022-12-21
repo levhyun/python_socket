bd21edc35fcc02fcd3f46dc3f52ebfc77f0b6c6a
lev._.hyun <110030247+levhyun@users.noreply.github.com>
2022-12-22 07:25:08 +0900

    re codeing

--- client.py
+++ client.py
@@ -1,40 +1,48 @@
 import socket
+import threading
 
-# Message 설정
-HEADER = 64
-# 최대 {HEADER}byte의 데이터(message)를 통신할 수 있다.
+def Send(client):
+    while True:
+        str = input()
+        if str != '':
+            message = "[" + NAME + "] " + str
+            # 사용자 입력
+            client.send(bytes(message.encode()))  
+            # Client -> Server 데이터 송신 
 
-# message 형식 설정
-FORMAT = 'utf-8'
+def Recv(client):
+    while True:
+        recv_data = client.recv(1024).decode()  
+        # Server -> Client 데이터 수신
+        print(recv_data)
 
-# 연결 해제 메세지 설정
-DISCONNECT_MESSAGE = "DISCONNECT!"
+if __name__ == '__main__':
+    # client 설정
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    # AF_INET -> 해당 소켓은 IPV4(IP version 4)로 사용을 의미
+    # SOCK_STREAM -> 해당 소켓에 TCP 패킷을 받겠다는 의미
 
-SERVER = input()
-PORT = int(input())
+    SERVER = input("IP:")
+    # 통신할 대상의 IP 주소
+    PORT = int(input("PORT:"))
+    # 통신할 대상의 Port 주소
 
-# Server Aress
-ADDR = (SERVER, PORT)
-# result : ('PC Adress(IPV4)', PORT(6060))
+    NAME = input("NAME:")
 
-# client 설정
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-# AF_INET -> 해당 소켓은 IPV4(IP version 4)로 사용을 의미
-# SOCK_STREAM -> 해당 소켓에 TCP 패킷을 받겠다는 의미
+    # Server Aress
+    ADDR = (SERVER, PORT)
+    # result : ('PC Adress(IPV4)', PORT(6060), 'NAME')
+    
+    # server에 연결
+    client.connect(ADDR)
+    # 서버로 연결시도
 
-# server에 연결
-client.connect(ADDR)
+    print(f'Connecting to {SERVER}:{PORT}')
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+    #Client의 메시지를 보낼 쓰레드
+    sendthread = threading.Thread(target=Send, args=(client, ))
+    sendthread.start()
 
-print(f"연결 해제 메세지 : {DISCONNECT_MESSAGE}\n")
-while True:
-    talk = input()
-    send(talk)    
+    #Server로 부터 다른 클라이언트의 메시지를 받을 쓰레드
+    recvthread = threading.Thread(target=Recv, args=(client, ))
+    recvthread.start()
--- server.py
+++ server.py
@@ -1,72 +1,116 @@
 import socket
 import threading
+from queue import Queue
 
-# Message 설정
-HEADER = 64
-# 최대 {HEADER}byte의 데이터(message)를 통신할 수 있다.
+# def Message_set(message):
+#     start = 0
+#     stop = 0
+#     for i in range(0,len(str(message))):
+#         if message[i] == '[':
+#             start = i
+#         if message[i] == ']':
+#             stop = i+1
+#     name = message[start:stop]
+#     message = message[stop:len(str(message))]
+#     message = message + name
+#     message_length = len(str(message))
+#     space = " "
+#     final_space = ""
+#     for i in range(0,146-message_length):
+#         final_space = final_space + space   
+#     final_message = final_space + message
+#     return final_message
 
-# PORT 지정
-PORT = 6060
+def Send(group, send_queue):
+    print('Thread Send Start')
+    while True:
+        try:
+            #새롭게 추가된 클라이언트가 있을 경우 Send 쓰레드를 새롭게 만들기 위해 루프를 빠져나감
+            recv = send_queue.get()
+            if recv == 'Group Changed':
+                print('Group Changed')
+                break
 
-# SERVER 설정
-SERVER = socket.gethostbyname(socket.gethostname())
-# socket.gethostname() -> PC Name
-# socket.gethostbyname(PC Name) -> IP Adress
 
-# Final Server Aress
-ADDR = (SERVER, PORT)
-# result : ('PC Adress(IPV4)', PORT(6060))
+            #for 문을 돌면서 모든 클라이언트에게 동일한 메시지를 보냄
+            for conn in group:
+                message = str(recv[0])
+                if recv[1] != conn: 
+                    #client 본인이 보낸 메시지는 받을 필요가 없기 때문에 제외시킴
+                    print(message)
+                    conn.send(bytes(message.encode()))
+                else:
+                    pass
+        except:
+            pass
 
-# message 형식 설정
-FORMAT = 'utf-8'
+def Recv(conn, count, send_queue):
+    print('Thread Recv(' + str(count) + ') Start\n')
+    while True:
+        message = conn.recv(1024).decode()
+        print(f"RECEIVE([{SERVER}:6060][Thread:{str(count)}]{message})")
+        # send_queue.put([Message_set(message), conn, count]) 
+        send_queue.put([message, conn, count]) 
+        #각각의 클라이언트의 메시지, 소켓정보, 쓰레드 번호를 send로 보냄
 
-# 연결 해제 메세지 설정
-DISCONNECT_MESSAGE = "DISCONNECT!"
+if __name__ == '__main__':
+    send_queue = Queue()
 
-# socket 설정
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-# AF_INET -> 해당 소켓은 IPV4(IP version 4)로 사용을 의미
-# SOCK_STREAM -> 해당 소켓에 TCP 패킷을 받겠다는 의미
+    # PORT 지정
+    PORT = 6060
 
-# 서버와 PORT 연결
-server.bind(ADDR)
-# 서버(PC Adress(IPV4))에 PORT(6060)를 연결
-# bind는 값을 튜플로 받기에 괄호가 두개이어야한다
-# result : server.bind(('PC Adress', 6060))
+    # SERVER 설정
+    SERVER = socket.gethostbyname(socket.gethostname())
+    # socket.gethostname() -> PC Name
+    # socket.gethostbyname(PC Name) -> IP Adress
 
-def Handle_client(conn, addr):
-    # conn -> 연결된 대상과 다시 통신할 수 있게 해주는 소켓 개체
-    # addr -> 연결 요청이 온 client의 ip adress와 PORT번호
-    print(f"※NEW CONNECTION※\n{addr} connected.\n")
-    connected = True
-    while connected:
-        message_length = conn.recv(HEADER).decode(FORMAT)
-        # recv(HEADER) -> client로부터 최대 {HEADER}byte까지의 데이터를 받는다
-        if message_length:    
-            message_length = int(message_length)
-            message = conn.recv(message_length).decode(FORMAT)
-            if message == DISCONNECT_MESSAGE:
-                print(f"{addr} disconnected.\n")
-                connected = False
-                # while문 종료
-            print(f"[{addr}] {message}")
-            conn.send("Message received.".encode(FORMAT))
-    conn.close()
-    # close() -> 연결 해제
+    # Final Server Aress
+    ADDR = (SERVER, PORT)
+    # result : ('PC Adress(IPV4)', PORT(6060))
 
-def start():
+    print(f"※STARTING※\nserver is starting......\nserver adress : {SERVER}:{PORT}\n")
+
+    # socket 설정
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    # AF_INET -> 해당 소켓은 IPV4(IP version 4)로 사용을 의미
+    # SOCK_STREAM -> 해당 소켓에 TCP 패킷을 받겠다는 의미
+
+    # 서버와 PORT 연결
+    server.bind(ADDR)
+    # 서버(PC Adress(IPV4))에 PORT(6060)를 연결
+    # bind는 값을 튜플로 받기에 괄호가 두개이어야한다
+    # result : server.bind(('PC Adress', 6060))
+
     server.listen()
     # server에 새로운 연결을 listen
+    # 소켓 연결, 여기서 파라미터는 접속수를 의미
     print(f"※LISTENING※\nserver is listening on {SERVER}\n")
+
+    count = 0 
+    # 쓰레드 번호 카운트
+    group = [] 
+    #연결된 클라이언트의 소켓정보를 리스트로 묶기 위함
+    
     while True:
-        conn, addr = server.accept()
-        # client에서 연결요청을 보내면 accept로 받고 return 값으로 소켓정보와 ip정보가 나온
-        thread = threading.Thread(target=Handle_client, args=(conn, addr))
-        thread.start()
-        print(f"※ACTIVE CONNECTIONS※\n{threading.activeCount()-1} threads running......")
-        # threading.activeCount() -> 현재 실행 중인 스레드 갯수
-        # -1을 하는 이유 : 처음 실행 되는 스레드는 항상 실행되어야 하기 때문에 
+        count += 1
+        conn, addr = server.accept()  
+        # 해당 소켓을 열고 대기
 
-print(f"※STARTING※\nserver is starting......\nserver adress : {SERVER}:{PORT}\n")
-start()
+        group.append(conn) 
+        #연결된 클라이언트의 소켓정보
+        print(f"※NEW CONNECTION※\n{str(addr)} connected.")
 
+        #소켓에 연결된 모든 클라이언트에게 동일한 메시지를 보내기 위한 쓰레드(브로드캐스트)
+        #연결된 클라이언트가 1명 이상일 경우 변경된 group 리스트로 반영
+        if count > 1:
+            send_queue.put('Group Changed')
+            sendthread = threading.Thread(target=Send, args=(group, send_queue))
+            sendthread.start()
+            pass
+        else:
+            sendthread = threading.Thread(target=Send, args=(group, send_queue))
+            sendthread.start()
+
+        #소켓에 연결된 각각의 클라이언트의 메시지를 받을 쓰레드
+        recvthread = threading.Thread(target=Recv, args=(conn, count, send_queue))
+        recvthread.start()
